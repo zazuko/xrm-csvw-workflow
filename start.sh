0ea21ca61133cf4b63f3040bd986f3bc8db16f64
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2021-07-30 16:42:33 +0200

    refactor: update setting environment

--- start.sh
+++ start.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-variables=()
-
-[[ -n "$INPUT_DIR" ]] && variables+=(--variable="inputDir=$INPUT_DIR")
-[[ -n "$OUTPUT" ]] && variables+=(--variable="targetFile=$OUTPUT")
-[[ -n "$MAPPINGS" ]] && variables+=(--variable="mappings=$MAPPINGS")
-
 yarn barnard59 \
   run \
-  "${variables[@]}" \
+  --variable MAPPINGS \
+  --variable TARGET_FILE \
   "$@" \
   --pipeline urn:pipeline:xrm#Main pipelines/main.ttl
